d45f0c932c39a2398a89191a2ee6f8c3a1f03cee
benie254 <davinci.monalissa@gmail.com>
2023-06-11 22:51:04 +0300

    remove unused database confs

--- deliver_proj/settings.py
+++ deliver_proj/settings.py
@@ -148,17 +148,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 WSGI_APPLICATION = 'deliver_proj.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
